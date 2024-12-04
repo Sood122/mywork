b757798c0f15006023807ecc6cd234e4aab70256
sood <108793220+Sood122@users.noreply.github.com>
2024-12-04 11:59:54 +0530

    Update day-1.py

--- winter_camp/day-1.py
+++ winter_camp/day-1.py
@@ -1,4 +1,4 @@
-# """
+# ""WAP to find sum of all natural number upto n"
 # def sum(a):
 #     if a < 1:
 #         return "Input should be a natural number (a >= 1)."
@@ -10,7 +10,7 @@
 # else:
 #     print("Please enter a natural number (a >= 1).")
 #     """
-   
+ #-------------WAP to find sum of odd number upto n------------------------------------  
 def sum_of_all_odd_number(n):
     total=0
     for num  in range(1,n+1):
@@ -20,7 +20,15 @@ def sum_of_all_odd_number(n):
 n=int(input("Enter the number:"))
 print("The sum of all odd numbers up to", n, "is:", sum_of_all_odd_number(n))
 
-#-----------------------------------------------------------------------------------------------------------
+#----------------------WAP to find the square of number n-------------------------------------------
+
+def square(n):
+    return n * n
+number = int(input("Enter a number: "))
+print(f"The square of {number} is {square(number)}")
+
+
+#---------------------------------------------WAP to find sum and square of odd number upto n--------------------------------------------------------------
 def sum_square_odd_numbers(n):
     num = (n + 1) // 2
     sum_odds = num ** 2
@@ -30,7 +38,7 @@ n = int(input("Enter the number: "))
 result = sum_square_odd_numbers(n)
 print(f"The square of the sum of odd numbers up to {n} is {result}")
 
-#-------------------------------------------------------------------------------------------------------------
+#--------------------------------------------WAP to find count digits of number n-----------------------------------------------------------------
 def count(num):
     count = 0
     while num != 0:
@@ -40,7 +48,7 @@ def count(num):
 n = int(input("Enter the number: "))
 print(f"The number {n} has {count(n)} digits.")
 
-#----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------WAP to find count and sum of digits of number n-----------------------------------------------------------------
 def count(num):
     count = 0
     while num != 0:
